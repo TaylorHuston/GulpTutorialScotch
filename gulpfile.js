c7d1b453fc3d06991c7615a640ca4454381b9ac7
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-31 20:23:11 -0700

    Add Sass comppilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,32 @@
 // grab our packages
-var gulp   = require('gulp'),
-    jshint = require('gulp-jshint');
+var gulp = require('gulp'),
+  jshint = require('gulp-jshint'),
+  sass = = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps');
 
-// define the default task and add the watch task to it
-gulp.task('default', ['watch']);
 
 // configure the jshint task
-gulp.task('jshint', function() {
+gulp.task('jshint', function () {
   return gulp.src('source/javascript/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-// configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-  gulp.watch('source/javascript/**/*.js', ['jshint']);
+
+gulp.task('build-css', function () {
+return gulp.src('source/scss/**/*.scss')
+  .pipe(sourcemaps.init()) // Process the original sources
+  .pipe(sass())
+  .pipe(sourcemaps.write()) // Add the map to modified source.
+  .pipe(gulp.dest('public/assets/stylesheets'));
 });
+});
+
+gulp.task('watch', function () {
+  gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
+});
+
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
